d48c3879d550bc30b1cd61836eb5294a3293b34b
goldentroll <Kcjjos@outlook.com>
2021-01-09 01:18:32 +0330

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import data from './data.js';
-
+import path from 'path';
 const app = express();
 
 app.get('/api/products', (req, res) => {
@@ -11,9 +11,12 @@ app.get('/api/products/:id', (req, res) => {
   res.send(data.products.find((x) => x._id === req.params.id));
 });
 
-app.get('/', (req, res) => {
-  res.send('Server is ready');
-});
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/dist')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/dist/index.html'))
+);
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
